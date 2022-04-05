313601df84705f6078086fffd2acecc552dbbff2
JustBecerra <justj.becerra@gmail.com>
2022-04-05 10:55:40 -0300

    estructuración de navbar

---
+++ client/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,10 @@
+import ButtonNavs from "./NavButtons/ButtonNavs"
+
+export default function NavBar(){
+    return(
+        <div>
+            <header>need a hotel? we got you.</header>
+            <ButtonNavs />
+        </div>
+    )
+}
---
+++ client/src/components/NavBar/NavButtons/ButtonNavs.tsx
@@ -0,0 +1,7 @@
+export default function ButtonNavs(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
