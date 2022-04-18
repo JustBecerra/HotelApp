7304393eb0ca4fc16e9debab852e80b06e690c60
JustBecerra <justj.becerra@gmail.com>
2022-04-18 15:40:51 -0300

    preparacion para request de informacion

--- client/src/components/NavBar/SearchBar/Modal/ModalComp.tsx
+++ client/src/components/NavBar/SearchBar/Modal/ModalComp.tsx
@@ -4,48 +4,72 @@ import Modal from 'react-modal';
 import { getProperties } from '../../../redux/Actions/getProperties';
 import { useDispatch } from 'react-redux';
 
+export interface ListType {
+  checkIn: string,
+  checkOut: string,
+  firstRoom: number,
+  secondRoom: number,
+  thirdRoom: number,
+}
+
+const listInfo = {
+  checkIn: '',
+  checkOut: '',
+  firstRoom: 0,
+  secondRoom: 0,
+  thirdRoom: 0,
+}
+
 export default function ModalComp(props:{searchV: string, modalIsOpen: boolean, closeModal: () => void, setSearch: (value: string) => void}) {
     const dispatch = useDispatch();
+    const [list,setList] = React.useState<ListType>(listInfo);
 
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
         e.preventDefault();
         try{
           if(props.searchV){
-            dispatch(await getProperties(props.searchV));
+            dispatch(await getProperties(props.searchV, list));
             props.setSearch('')
           } 
         }catch(err){
           console.log(err)
         }
-      }
+    }
+    
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>){
+        setList({
+            ...list,
+            [e.target.name]: e.target.value
+        })
+    }
+      
+    
 
     return(
         <>
           <Modal
-              isOpen={props.modalIsOpen}
-              onRequestClose={props.closeModal}
-              ariaHideApp={false}
-              className='ModalBG'
-                
-            >
-                <form onSubmit={(e) => handleSubmit(e)} className='OverlayModal'>
-                  <label className='checkin'>Check-in date</label>
-                  <input type='date' className='inputModal' placeholder='check-in date DD-MM-YY'/>
-                  <label className='checkout'>Check-out date</label>
-                  <input type='date' className='inputModal' placeholder='check-out date DD-MM-YY'/>
-                  <input type='number' className='inputModal' placeholder='number of guests in first room'/>
-                  <input type='number' className='inputModal' placeholder='number of guests in second room'/>
-                  <input type='number' className='inputModal' placeholder='number of guests in third room'/>
-                  <input type='text' className='inputModal' placeholder='age of children in first room'/>
-                  <input type='text' className='inputModal' placeholder='age of children in second room'/>
-                  <input type='text' className='inputModal' placeholder='age of children in third room'/>
-                  <input type='text' className='inputModal' placeholder='rating'/>
-                  <input type='number' className='inputModal' placeholder='min price'/>
-                  <input type='number' className='inputModal' placeholder='max price'/>
-                  <button type='submit' className='closeButton'>Search</button>
-                </form>
-                
-            </Modal>
+            isOpen={props.modalIsOpen}
+            onRequestClose={props.closeModal}
+            ariaHideApp={false}
+            className='ModalBG'
+          >
+            <form onSubmit={(e) => handleSubmit(e)} className='OverlayModal'>
+              <label className='checkin'>Check-in date</label>
+              <input type='date' onChange={(e) => handleChange(e)} name='checkIn' className='inputModal' placeholder='check-in date DD-MM-YY'/>
+              <label className='checkout'>Check-out date</label>
+              <input type='date' onChange={(e) => handleChange(e)} name='checkOut' className='inputModal' placeholder='check-out date DD-MM-YY'/>
+              <input type='number' onChange={(e) => handleChange(e)} name='firstRoom' className='inputModal' placeholder='number of guests in first room'/>
+              <input type='number' onChange={(e) => handleChange(e)} name='secondRoom' className='inputModal' placeholder='number of guests in second room'/>
+              <input type='number' onChange={(e) => handleChange(e)} name='thirdRoom' className='inputModal' placeholder='number of guests in third room'/>
+              <input type='text' onChange={(e) => handleChange(e)} name='firstChildren' className='inputModal' placeholder='age of children in first room'/>
+              <input type='text' onChange={(e) => handleChange(e)} name='secondChildren' className='inputModal' placeholder='age of children in second room'/>
+              <input type='text' onChange={(e) => handleChange(e)} name='thirdChildren' className='inputModal' placeholder='age of children in third room'/>
+              <input type='text' onChange={(e) => handleChange(e)} name='rating' className='inputModal' placeholder='rating'/>
+              <input type='number' onChange={(e) => handleChange(e)} name='minPrice' className='inputModal' placeholder='min price'/>
+              <input type='number' onChange={(e) => handleChange(e)} name='maxPrice' className='inputModal' placeholder='max price'/>
+              <button type='submit' className='closeButton'>Search</button>
+            </form>
+          </Modal>
         </>
     )
 }
--- client/src/components/redux/Actions/getProperties.tsx
+++ client/src/components/redux/Actions/getProperties.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import { GET_LOCATIONS } from '../types/actionTypes'
+import { ListType } from '../../NavBar/SearchBar/Modal/ModalComp'
 
-export async function getProperties(cityname: string) {
-  let info = await axios.get('http://localhost:3001/locations/search/' + cityname)
+export async function getProperties(searchV: string, list: ListType) {
+  let info = await axios.get('http://localhost:3001/locations/search/' + searchV)
   let destinationArray = []
   for(var i=0; i < info.data.suggestions[1].entities.length; i++){
     destinationArray.push(info.data.suggestions[1].entities[i].destinationId)
